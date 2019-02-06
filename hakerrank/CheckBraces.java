43c40d8d0f3478d84eb8bea601a25a1526f86473
Sarthak Poshattiwar <sarthak.poshattiwar@gmail.com>
2019-02-06 10:26:19 -0500

    checkbraces added

---
+++ hakerrank/CheckBraces.java
@@ -0,0 +1,54 @@
+package hakerrank;
+
+import java.util.Stack;
+
+/**
+ *
+ * @author sarthak
+ */
+public class CheckBraces {
+    public static void main(String[] args) {
+        String s = new String("[[{}]]");
+        
+        int l= s.length();
+       
+        System.out.println( CheckBraces.balanced(l, s)? true :false);
+     
+       
+    }
+    
+    private static boolean checkEqual(char c, char p) {
+        
+        if(c=='(' && p==')') return true;
+        if(c=='{' && p=='}') return true;
+        if(c=='[' && p==']') return true;
+        else  return false;
+    }
+    public static boolean balanced(int l, String s)
+    {
+        
+         Stack<Character> st = new Stack();
+         for(int i = 0 ; i<l;i++){
+            if(s.charAt(i)== '(' ||s.charAt(i)== '[' || s.charAt(i)== '{' ){
+                st.push(s.charAt(i));
+                
+            }
+            if(s.charAt(i)== ')' ||s.charAt(i)== ']' || s.charAt(i)== '}' ){
+              
+                if(checkEqual(s.charAt(i), st.peek())){
+                    
+                    return false;
+                    
+                }
+                else{
+                    
+                   char t = st.pop();
+                }
+            }
+            
+        }
+         return (st.empty())?  true: false;
+    }
+    
+    
+}
