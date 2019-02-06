067c002e8e2c67b14cccdced7654b24909bc1e6e
Sarthak Poshattiwar <sarthak.poshattiwar@gmail.com>
2019-02-06 11:36:21 -0500

    corrected Checkbraces

--- hakerrank/CheckBraces.java
+++ hakerrank/CheckBraces.java
@@ -1,20 +1,16 @@
 package hakerrank;
-
 import java.util.Stack;
-
 /**
  *
  * @author sarthak
  */
 public class CheckBraces {
     public static void main(String[] args) {
-        String s = new String("[[{}]]");
+        String s = new String("[[{}]][{]}");
         
         int l= s.length();
        
-        System.out.println( CheckBraces.balanced(l, s)? true :false);
-     
-       
+        System.out.println( CheckBraces.balanced(l, s)? true :false);   
     }
     
     private static boolean checkEqual(char c, char p) {
@@ -30,20 +26,17 @@ public class CheckBraces {
          Stack<Character> st = new Stack();
          for(int i = 0 ; i<l;i++){
             if(s.charAt(i)== '(' ||s.charAt(i)== '[' || s.charAt(i)== '{' ){
-                st.push(s.charAt(i));
-                
+                st.push(s.charAt(i));            
             }
+            
             if(s.charAt(i)== ')' ||s.charAt(i)== ']' || s.charAt(i)== '}' ){
               
-                if(checkEqual(s.charAt(i), st.peek())){
-                    
-                    return false;
-                    
-                }
-                else{
-                    
+                if(checkEqual( st.peek(), s.charAt(i))){
+                
                    char t = st.pop();
+                   continue;
                 }
+                
             }
             
         }
